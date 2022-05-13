903261cc4114fad9f42729c8b135f19fbec53085
Rashid <rassh1512@gmail.com>
2022-05-13 18:40:40 +0530

    creating model and register it to admin site

--- TodoAPI/todoapp/models.py
+++ TodoAPI/todoapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    tid = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    desc = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
