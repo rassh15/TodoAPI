8691a44789aec688089d7600504b4221778960ec
Rashid <rassh1512@gmail.com>
2022-05-13 18:48:58 +0530

    creating URL file

---
+++ TodoAPI/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from account.views import RegisterAPI, LogoutAPI, CustomTokenObtainPairView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
+
+urlpatterns = [
+    path('api/login/', CustomTokenObtainPairView.as_view(), name='token_login'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterAPI.as_view(), name = "registerapi"),
+    path('api/logout', LogoutAPI.as_view(), name= "logoutapi"),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    ]   
