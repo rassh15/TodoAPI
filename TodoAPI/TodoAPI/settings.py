27ea62adf5a3ece0262b024ae247224f76ad7819
Rashid <rassh1512@gmail.com>
2022-05-13 20:22:44 +0530

    Configure setting for deployment on heroku

--- TodoAPI/TodoAPI/settings.py
+++ TodoAPI/TodoAPI/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -165,3 +168,6 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+#Heroku setting
+django_heroku.settings(locals())
